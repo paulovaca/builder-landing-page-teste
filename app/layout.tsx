582f5c2451f472606ff9e5ae969f3c4935a2c5a1
PaM <paulo.vaca@gmail.com>
2025-11-21 19:09:13 -0300

    chore: export Landing Page Teste from web builder

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,10 @@ export default function RootLayout({
       <head>
         <link
           rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Akshar:wght@400;500;600;700&display=swap"
+        />
+        <link
+          rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
         />
         <link
